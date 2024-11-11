dea544acf78a96a24e0ffd39f309dfcab7ee7d45
amirul1994 <amirulbrinto90@gmail.com>
2024-11-11 23:25:25 +0600

    push docker image

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(cors());
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+    serverSelectionTimeoutMS: 30000
 }, function(err) {
     if (err) {
         console.log("error!! " + err);
